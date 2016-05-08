206e61b18483644954843262bef677e059312d23
jmone15 <monejurgen@gmail.com>
2016-05-08 03:54:26 +0300

    jsfix

--- src/main/webapp/resources/js/scheduledVisit.js
+++ src/main/webapp/resources/js/scheduledVisit.js
@@ -1,14 +1,14 @@
 //confirm before deleting a user
 $(".deleteVisit").click(function (e) {
-    console.log($("body").attr('data-url') + 'ScheduledVisits/delete/' + $(this).attr('data-id'))
+    console.log($("body").attr('data-url') + 'scheduledVisits/delete/' + $(this).attr('data-id'))
 
     if (confirm('Are you sure you want to delete the scheduled visit?')) {
 
         $.ajax({
-            url: $("body").attr('data-url') + 'ScheduledVisits/delete/' + $(this).attr('data-id'),
+            url: $("body").attr('data-url') + 'scheduledVisits/delete/' + $(this).attr('data-id'),
             type: 'DELETE'
         }).done(function (data) {
-            window.location.href = $("body").attr('data-url') + "ScheduledVisits/";
+            window.location.href = $("body").attr('data-url') + "scheduledVisits/";
         });
     }
 });
