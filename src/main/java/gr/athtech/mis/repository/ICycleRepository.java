a97e71c15297c7564255246ea29857496d76bc4d
jmone15 <monejurgen@gmail.com>
2016-05-21 15:16:39 +0300

    JPQL for cycle id

--- src/main/java/gr/athtech/mis/repository/ICycleRepository.java
+++ src/main/java/gr/athtech/mis/repository/ICycleRepository.java
@@ -6,7 +6,9 @@
 package gr.athtech.mis.repository;
 
 import gr.athtech.mis.model.Cycle;
+import java.sql.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,4 +18,6 @@ import org.springframework.stereotype.Repository;
 @Repository("iCycleRepository")
 public interface ICycleRepository extends JpaRepository<Cycle, Long>{
     
+    @Query("SELECT c.id FROM Cycle as c WHERE endDate >= ?1 AND startDate <= ?1")
+    Long getCurrentCycleId(Date date);
 }
